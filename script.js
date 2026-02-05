3c49a2e2fc5af3d2264489048574a3d5a93c1640
Akintunde Kayode <kayodeakintunde919@gmail.com>
2026-02-05 22:01:44 +0100

    Pushing my code to github first time

---
+++ script.js
@@ -0,0 +1,63 @@
+function openMenu() {
+  document.querySelector(".menu-dropdown").style.height = "100%";
+}
+
+function closeMenu() {
+  document.querySelector(".menu-dropdown").style.height = "0";
+}
+
+//Scroll to the top
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+///////
+
+const columns = document.querySelectorAll(".process-col");
+columns.forEach((col) => {
+  col.addEventListener("mouseenter", () => {
+    // Remove active class from all
+    columns.forEach((c) => c.classList.remove("expand"));
+    // Add to current
+    col.classList.add("expand");
+  });
+});
+
+// Faq
+
+const faqTabs = document.querySelectorAll(".faq-tab");
+faqTabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const body = tab.querySelector(".faq-body");
+    const isOpen = body.classList.contains("open");
+    document
+      .querySelectorAll(".faq-body")
+      .forEach((f) => f.classList.remove("open"));
+
+    //  If it wasn't open, open it now
+    if (!isOpen) {
+      body.classList.add("open");
+    }
+  });
+});
